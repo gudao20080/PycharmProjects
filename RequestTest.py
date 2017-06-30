a54793f97395c77c4aff8fa3d343454e1c6db4d4
WangKai <275892441>
2017-06-30 18:00:24 +0800

    spider

--- RequestTest.py
+++ RequestTest.py
@@ -1,12 +1,56 @@
+from http.cookiejar import CookieJar, FileCookieJar
 
 import requests
+import re
+import scrapy
+from urllib import request
+import urllib
+import chardet
+from http.cookiejar import FileCookieJar, MozillaCookieJar, CookieJar
 
 
 if __name__ == "__main__":
-    payload = {'key1': 'value1', 'key2': 'value2'}
-    r = requests.get("http://httpbin.org/get", params=payload)
-    print(r.url)
-    print(r.text)
-    print(r.encoding)
-    print(r.content)
-    print(r.json())
+    url = "http://cuiqingcai.com/954.html"
+    url = "https://www.baidu.com/"
+    # url = "http://www.whatismyip.com.tw/"
+
+    # req = request.Request(url)
+    # req.set_proxy("")
+    # req.headers = {}
+    # req.method
+
+    # cookie_jar = CookieJar()
+    file_name = 'D:/cookie.txt'
+    cookie_jar = MozillaCookieJar(file_name)
+
+    #创建cookieJar处理器
+    cookiejar_processor = request.HTTPCookieProcessor(cookie_jar)
+    # #通过cookieJar处理器来创建opener
+    # opener = request.build_opener(proxy_handler)
+    # opener.open(url)
+
+    proxy_handler = request.ProxyHandler(proxies={"http":"61.163.39.70:9999"})
+    build_opener = request.build_opener(proxy_handler, cookiejar_processor)
+    build_opener.addheaders = [('User-Agent',
+                          'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36')]
+
+    # opener_open = build_opener.open(url)
+    request.install_opener(build_opener)
+
+    urlopen = request.urlopen(url, timeout=10)
+    open_read = urlopen.read()
+    encoding_ = chardet.detect(open_read)['encoding']
+
+    if not encoding_:
+        encoding_ = 'utf-8'
+    data = open_read.decode(encoding_)
+    # print(data)
+    # for item in cookie_jar:
+    #     print(item.name, "---------------", item.value)
+
+    cookie_jar.save()
+    mozilla_cookie_jar = MozillaCookieJar(file_name)
+    mozilla_cookie_jar.load()
+    print(mozilla_cookie_jar)
+    for item in mozilla_cookie_jar:
+        print(item.name, item.value)
--- reTest.py
+++ reTest.py
@@ -1,33 +1,18 @@
 import re
 
+s = '33one1two2three3four4'
 
-# 将正则表达式编译成Pattern对象
-pattern = re.compile(r'hello')
+pattern = re.compile(r'(\d+)+')
+result = re.findall(pattern, s)
 
-
-result1 = re.match(pattern, 'hello')
-result2 = re.match(pattern, 'helloo CQC!')
-result3 = re.match(pattern, 'helo CQC!')
-result4 = re.match(pattern, 'hello CQC!')
-
-
-def print_result(name, result):
-    if result:
-        print(name, result.group())
-    else:
-        print(name, '匹配失败')
+match = re.match("\d", s)
+# print(result)
+print(match.re)
+print(match.pos)
+print(match.endpos)
+print(match.lastgroup)
 
 
-print_result('result1', result1)
-print_result('result2', result2)
-print_result('result3', result3)
-print_result('result4', result4)
 
 
-print(result1.string)
-print(result1.re)
-print(result1.pos)
-print(result2.endpos)
-print(result2.lastindex)
-print(result2.lastgroup)
 
